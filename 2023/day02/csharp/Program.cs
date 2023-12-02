03b7a283019e2467bec83c07e013dcebf801bb41
Antonio Subašić <antonio.subasic.public@gmail.com>
2023-12-02 12:27:47 +0100

    added 2023/day02/csharp

---
+++ 2023/day02/csharp/Program.cs
@@ -0,0 +1,74 @@
+﻿int Part1(Game[] input)
+{
+    var idsSum = 0;
+
+    foreach (var game in input)
+    {
+        var valid = true;
+
+        foreach (var subset in game.CubeSubsets)
+        {
+            valid = subset.All(cube => cube.amount <= (int)cube.color);
+            if (!valid) { break; }
+        }
+
+        if (valid) { idsSum += game.Id; }
+    }
+
+    return idsSum;
+}
+
+int Part2(Game[] input) => input.Sum(game => game.GetPowerOf(Color.Red) * game.GetPowerOf(Color.Green) * game.GetPowerOf(Color.Blue));
+
+
+var input = File.ReadAllLines("../input.txt")
+    .Select(line =>
+    {
+        var splitted = line.Split(':');
+
+        var game = int.Parse(splitted[0].Split(' ')[^1]);
+        var subsets = new List<(int, Color)[]>();
+
+        foreach (var subset in splitted[1].Trim().Split(';'))
+        {
+            var cubes = new List<(int, Color)>();
+
+            foreach (var cubeItem in subset.Split(','))
+            {
+                var cubeTuple = cubeItem.Trim().Split(' ');
+
+                cubes.Add(
+                    (
+                        int.Parse(cubeTuple[0]),
+                        cubeTuple[1] switch
+                        {
+                            "blue" => Color.Blue,
+                            "red" => Color.Red,
+                            "green" => Color.Green,
+                            _ => throw new Exception("Unknown color")
+                        }
+                    )
+                );
+            }
+
+            subsets.Add(cubes.ToArray());
+        }
+        
+        return new Game(game, subsets.ToArray());
+    }).ToArray();
+
+Console.WriteLine(Part1(input));
+Console.WriteLine(Part2(input));
+
+
+record Game(int Id, (int amount, Color color)[][] CubeSubsets)
+{
+    public int GetPowerOf(Color color) => CubeSubsets.Max(subset => subset.Where(cube => cube.color == color).Sum(cube => cube.amount));
+}
+
+enum Color
+{
+    Red = 12,
+    Green = 13,
+    Blue = 14,
+}
