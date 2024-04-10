21d3a63a57a7804b0ce30cab359125a4aad030e0
Antonio Subašić <antonio.subasic.public@gmail.com>
2024-04-10 22:14:59 +0200

    updated runtime added method `has_init_command` added method `has_compile_command` and `get_compile_arguments` added separate compilation before execution added execution benchmark

--- runtime/src/proj.rs
+++ runtime/src/proj.rs
@@ -54,6 +54,10 @@ impl Language {
         }
     }
 
+    pub fn has_init_command(&self) -> bool {
+        self.name != "python"
+    }
+
     pub fn get_init_arguments(&self, directory: &Path) -> Vec<String> {
         match self.name.as_str() {
             "csharp" => vec![
@@ -67,6 +71,17 @@ impl Language {
                 "-q".to_string(),
                 directory.to_str().unwrap().to_string(),
             ],
+            _ => panic!("invalid language"),
+        }
+    }
+
+    pub fn has_compile_command(&self) -> bool {
+        self.name == "rust"
+    }
+
+    pub fn get_compile_arguments(&self) -> Vec<String> {
+        match self.name.as_str() {
+            "rust" => vec!["build".to_string(), "--release".to_string()],
             _ => panic!("invalid language"),
         }
     }
@@ -132,7 +147,7 @@ impl Project {
 
             let base_file = home.join(".config/aoc").join(self.language.get_base_file());
 
-            if self.language.name != "python" {
+            if self.language.has_init_command() {
                 let status = Command::new(self.language.get_command())
                     .args(self.language.get_init_arguments(language_directory))
                     .stdout(Stdio::null())
@@ -165,7 +180,24 @@ impl Project {
             fs::write(&input_file, input)?;
         }
 
-        let run_command = Command::new(self.language.get_command())
+        if self.language.has_compile_command() {
+            let compile_command = Command::new(self.language.get_command())
+                .args(self.language.get_compile_arguments())
+                .current_dir(self.directory.join(&self.language.name))
+                .output()?;
+
+            if !compile_command.status.success() {
+                return Err(format!(
+                    "failed to compile project: {}",
+                    String::from_utf8_lossy(&compile_command.stderr)
+                )
+                .into());
+            }
+        }
+
+        let run_command = Command::new("time")
+            .arg("-p")
+            .arg(self.language.get_command())
             .args(self.language.get_run_arguments(&self.directory))
             .current_dir(self.directory.join(&self.language.name))
             .output()?;
@@ -214,10 +246,14 @@ impl Project {
                 part2_correct = part2 == solution.lines().nth(1).unwrap();
             }
 
+            let execution_time_string = String::from_utf8_lossy(&run_command.stderr);
+            let execution_time: Vec<&str> = execution_time_string.lines().nth(2).unwrap().split(' ').nth(1).unwrap().split(':').collect();
+
             Ok(format!(
-                "{}\n{}",
+                "{}\n{}\n\n{}",
                 if part1_correct { part1.green() } else { part1.red() },
-                if part2_correct { part2.green() } else { part2.red() }
+                if part2_correct { part2.green() } else { part2.red() },
+                format!("{}h {}m {}s", execution_time.get(2).unwrap_or(&"0"), execution_time.get(1).unwrap_or(&"0"), execution_time.get(0).unwrap_or(&"0"))
             ))
         }
     }
