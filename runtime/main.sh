0b6ad2cf3bbd120be06e504c7aa88a60b95ac4e0
Antonio Subašić <antonio.subasic.as@gmail.com>
2024-09-05 10:17:19 +0200

    added path handling via commandline-parameters and cwd-auto-detection

--- runtime/main.sh
+++ runtime/main.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+config_path="$HOME/.config/aoc"
+
 year=""
 day=""
 language=""
@@ -40,6 +42,13 @@ while getopts ":y:d:l:" opt; do
             day="$OPTARG"
             ;;
         l)
+            if [[ "$language" != "" ]]; then
+                echo "language already set" >&2
+                exit 1
+            elif [[ ! $OPTARG =~ ^[a-zA-Z]+$ ]]; then
+                echo "invalid language: $OPTARG" >&2
+                exit 1
+            fi
             language="$OPTARG"
             ;;
         \?)
@@ -56,4 +65,72 @@ shift $((OPTIND - 1))
 
 if [[ $# -gt 0 ]]; then
     mode="$1"
+fi
+
+path_pattern=$(sed "s|^~|$HOME|" "$config_path/path")
+path_pattern="${path_pattern%/}"
+
+path_regex=$(echo "$path_pattern" | \
+    # escape slashes
+    sed 's/\//\\\//g' | \
+    # replace {year} with 4-digit number regex group
+    sed 's/{year}/([0-9]\{4\})/' | \
+    # replace {day:n} with a n-digit padded number regex group
+    sed -E 's/\{day:([0-9]+)\}/([0-9]\{\1\})/' | \
+    # replace {day} with 1 or 2-digit number regex group
+    sed 's/{day}/([0-9]{1,2})/' | \
+    # replace {language} with characters regex group
+    sed 's/{language}/([a-zA-Z]+)/')
+
+path_regex_matches=0
+year_regex_match=""
+day_regex_match=""
+language_regex_match=""
+if [[ "$PWD/" =~ $path_regex ]]; then
+    path_regex_matches=1
+    year_regex_match="${BASH_REMATCH[1]}"
+    day_regex_match="${BASH_REMATCH[2]}"
+    language_regex_match="${BASH_REMATCH[3]}"
+fi
+
+path="$path_pattern"
+
+if [[ "$year" != "" ]]; then
+    path=${path/\{year\}/$year}
+elif [[ $path_regex_matches -eq 1 ]]; then
+    path=${path/\{year\}/$year_regex_match}
+    year=$year_regex_match
+else
+    echo "year not set" >&2
+    exit 1
+fi
+
+if [[ "$day" != "" ]]; then
+    if [[ "$path" =~ \{day:([0-9]+)\} ]]; then
+        day_length="${BASH_REMATCH[1]}"
+        path=${path/\{day:$day_length\}/$(printf "%0${day_length}d" $day)}
+    else
+        path=${path/\{day\}/$day}
+    fi
+elif [[ $path_regex_matches -eq 1 ]]; then
+    path=$(echo "$path" | sed -E "s/\{day(:[0-9]+)?\}/$day_regex_match/")
+    day=$day_regex_match
+else
+    echo "day not set" >&2
+    exit 1
+fi
+
+if [[ "$language" != "" ]]; then
+    path=${path/\{language\}/$language}
+elif [[ $path_regex_matches -eq 1 ]]; then
+    path=${path/\{language\}/$language_regex_match}
+    language=$language_regex_match
+else
+    echo "language not set" >&2
+    exit 1
+fi
+
+if [[ ! -d "$path" ]]; then
+    echo "path does not exist: $path" >&2
+    exit 1
 fi
