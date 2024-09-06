d29d243f75770a3a10de19ea98567a366c94ba2d
Antonio Subašić <antonio.subasic.public@gmail.com>
2024-09-06 13:18:32 +0200

    implemented solution evaluation via api

--- runtime/main.sh
+++ runtime/main.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+NC='\033[0m'
+
 throw() {
     local message="$1"
     local error_code="${2:-1}"
@@ -15,6 +19,92 @@ fetch_input() {
     local response=$(curl -s -H "Cookie: session=${cookie}" "https://adventofcode.com/${year}/day/${day}/input")
 
     echo "$response"
+}
+
+# return codes
+# 0: right answer
+# 1: wrong answer
+# 2: cooldown
+submit_answer() {
+    local year="$1"
+    local day="$2"
+    local part="$3"
+    local answer="$4"
+    local cookie="$5"
+
+    local response=$(curl -s -X POST -H "Cookie: session=${cookie}" -d "level=${part}&answer=${answer}" "https://adventofcode.com/${year}/day/${day}/answer")
+
+    if [[ "$response" =~ "That's the right answer!" ]]; then
+        return 0
+    elif [[ "$response" =~ "You don't seem to be solving the right level" ]]; then
+        local day_response=$(curl -s -H "Cookie: session=${cookie}" "https://adventofcode.com/${year}/day/${day}")
+        local correct_answer=$(echo "$day_response" | grep -oP '<p>Your puzzle answer was <code>\K(.*?)(?=</code>)' | sed -n "${part}p")
+        [[ "$correct_answer" -eq "$answer" ]]
+        return $?
+    elif [[ "$response" =~ "You gave an answer too recently" ]]; then
+        local cooldown_time=$(echo "$response" | grep -oP 'You have \K(.*?)(?= left to wait)')
+        echo "cooldown left: $cooldown_time" >&2
+        return 2
+    else
+        return 1
+    fi
+}
+
+# return codes
+# 0: both solutions incorrect
+# 1: part 1 correct
+# 2: part 2 correct
+# 3: both solutions correct
+evaluate_solution() {
+    local solution_file="$1"
+    local year="$2"
+    local day="$3"
+    local solution_part1="$4"
+    local solution_part2="$5"
+    local cookie="$6"
+
+    if [[ -f "$solution_file" ]]; then
+        local solution_content
+        solution_content=$(<"$solution_file")
+
+        local part1=$(echo "$solution_content" | sed -n '1p')
+        local part2=$(echo "$solution_content" | sed -n '2p')
+
+        local return_code=0
+
+        if [[ "$part1" == "$solution_part1" ]]; then
+            return_code=1
+        fi
+
+        if [[ "$part2" == "$solution_part2" ]]; then
+            return_code=$((return_code + 2))
+        fi
+
+        return $return_code
+    else
+        part1_submit_output=$(submit_answer "$year" "$day" 1 "$solution_part1" "$cookie" 2>&1)
+        part1_submit_result=$?
+        if [[ $part1_submit_result -eq 2 ]]; then
+            throw "$part1_submit_output"
+        fi
+
+        part2_submit_output=$(submit_answer "$year" "$day" 2 "$solution_part2" "$cookie" 2>&1)
+        part2_submit_result=$?
+        if [[ $part2_submit_result -eq 2 ]]; then
+            throw "$part2_submit_output"
+        fi
+
+        if [[ $part1_submit_result -eq 0 && $part2_submit_result -eq 0 ]]; then
+            echo -e "$solution_part1\n$solution_part2" > "$solution_file"
+            return 3
+        elif [[ $part1_submit_result -eq 0 ]]; then
+            return 1
+        elif [[ $part2_submit_result -eq 0 ]]; then
+            return 2
+        else
+            return 0
+        fi
+    fi
 }
 
 config_path="$HOME/.config/aoc"
@@ -140,10 +230,10 @@ fi
 case $mode in
     run)
         input_file_path="$(dirname $path)/input.txt"
+        solution_file_path="$(dirname $path)/.solution.txt"
+        cookie=$(sed 's/\n$//' "$config_path/cookie")
 
         if [[ ! -f "$input_file_path" ]]; then
-            cookie=$(sed 's/\n$//' "$config_path/cookie")
-
             if [[ "$cookie" == "" ]]; then
                 throw "cookie not set"
             else
@@ -177,7 +267,17 @@ case $mode in
                 ;;
         esac
 
-        echo "$output"
+        part1=$(echo "$output" | sed -n '1p')
+        part2=$(echo "$output" | sed -n '2p')
+
+        evaluate_solution "$solution_file_path" "$year" "$day" "$part1" "$part2" "$cookie"
+        evaluation=$?
+
+        part1_color=$([[ $evaluation -eq 1 || $evaluation -eq 3 ]] && echo $GREEN || echo $RED)
+        part2_color=$([[ $evaluation -eq 2 || $evaluation -eq 3 ]] && echo $GREEN || echo $RED)
+
+        echo -e "${part1_color}$part1${NC}"
+        echo -e "${part2_color}$part2${NC}"
         ;;
     *)
         throw "invalid mode: $mode"
