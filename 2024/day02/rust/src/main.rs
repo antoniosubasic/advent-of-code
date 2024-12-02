9ef0445c860a03d542e97be5d10037cbf03ccee5
Antonio Subašić <antonio.subasic.as@gmail.com>
2024-12-02 12:56:01 +0100

    added 2024/day02/rust

---
+++ 2024/day02/rust/src/main.rs
@@ -0,0 +1,51 @@
+use std::fs;
+
+fn evaluate_report(report: &Vec<i32>, removable: bool) -> bool {
+    let mut invalids = 0;
+
+    let ascending = report[1] > report[0];
+
+    for i in 0..report.len() - 1 {
+        let diff = report[i + 1] - report[i];
+        if !(1..=3).contains(&diff.abs()) || ascending && diff < 0 || !ascending && diff > 0 {
+            invalids += 1;
+        }
+    }
+
+    match invalids {
+        0 => true,
+        _ => {
+            removable && {
+                for i in 0..report.len() {
+                    let mut report = report.clone();
+                    report.remove(i);
+                    if evaluate_report(&report, false) {
+                        return true;
+                    }
+                }
+
+                false
+            }
+        }
+    }
+}
+
+fn main() {
+    let input: Vec<Vec<i32>> = fs::read_to_string("../input.txt")
+        .unwrap()
+        .lines()
+        .map(|line| line.split(' ').map(|num| num.parse().unwrap()).collect())
+        .collect();
+
+    println!(
+        "{}\n{}",
+        input
+            .iter()
+            .filter(|report| evaluate_report(report, false))
+            .count(),
+        input
+            .iter()
+            .filter(|report| evaluate_report(report, true))
+            .count()
+    );
+}
